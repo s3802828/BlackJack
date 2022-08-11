a700ff392822bf8608b4f39a5bef23818d757c83
Giang Le Quynh <s3802828@rmit.edu.vn>
2022-08-11 23:58:05 +0700

    Initial Commit

---
+++ BlackJack/BlackJackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJackApp.swift
+//  BlackJack
+//
+//  Created by Giang Le on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BlackJackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
